13031113a7bbb445c5273ed43bd8b89b60e0901c
hungtidus <hungtidus@gmail.com>
2024-11-08 07:36:59 +0000

    update version 3

--- src/type/custom.d.ts
+++ src/type/custom.d.ts
@@ -1,10 +1,10 @@
 declare module '@n8n_io/riot-tmpl' {
     const content: any;
     export = content;
-}
-
-declare module '@langchain/core/callbacks/manager' {
+  }
+  
+  declare module '@langchain/core/callbacks/manager' {
     export interface CallbackManager {
-        // Add any necessary interface properties
+      // Add any necessary interface properties
     }
-}
+  }
